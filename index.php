f3db33fb6c01eef041069a652f20b4cdb37014df
Elogeek <elodiechristin@gmail.com>
2021-01-18 13:23:51 +0100

    first exo PHP (hello...)

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
-<?php
-
-// Notre premier Hello World
-
-     "Hello World !";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Fisrt exo PHP</title>
+</head>
+<body>
+    <?php
+        // Notre premier Hello World
+         echo "Hello World !";
+         // ou
+        echo"<br><span>Hello world !</span>";
+    ?>
+</body>
+</html>
